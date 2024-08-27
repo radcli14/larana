b33baacb1b11d1334b50a3f6ab094fe8281857bd
radcli14 <radcli14@gmail.com>
2024-08-27 23:45:52 +0200

    added the La Rana scene to the ARKit app

--- JuegoDeLaRana/JuegoDeLaRana/ContentView.swift
+++ JuegoDeLaRana/JuegoDeLaRana/ContentView.swift
@@ -20,15 +20,16 @@ struct ARViewContainer: UIViewRepresentable {
         
         let arView = ARView(frame: .zero)
 
-        // Create a cube model
-        let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
-        let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
-        let model = ModelEntity(mesh: mesh, materials: [material])
-        model.transform.translation.y = 0.05
-
         // Create horizontal plane anchor for the content
         let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
-        anchor.children.append(model)
+
+        // Load the La Rana scene
+        if let larana = try? ModelEntity.loadModel(named: "Scene") {
+            anchor.children.append(larana)
+            if let laranaAnchor = larana.anchor {
+                arView.scene.anchors.append(laranaAnchor)
+            }
+        }
 
         // Add the horizontal plane anchor to the scene
         arView.scene.anchors.append(anchor)
