4f574b981194d13924c47d1541f15d085be2b20e
radcli14 <radcli14@gmail.com>
2024-09-05 19:37:03 +0200

    gave a more modern appearance to the overlay on the bottom of the screen

--- JuegoDeLaRana/JuegoDeLaRana/OverlayView.swift
+++ JuegoDeLaRana/JuegoDeLaRana/OverlayView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+/// Overlay of the user controls and scoring displayed on the bottom of the screen
 struct OverlayView: View {
     let state: GameState
     let nThrows: Int
@@ -18,67 +19,102 @@ struct OverlayView: View {
     var body: some View {
         VStack {
             Spacer()
-            scoreBoard
             HStack(alignment: .center, spacing: Constants.buttonSpacing) {
                 resetAnchorButton
+                scoreBoard
                 moveTableButton
+            }
+            .padding(Constants.backgroundPadding)
+            .background {
+                RoundedRectangle(cornerRadius: Constants.backgroundRadius)
+                    .foregroundColor(Color(UIColor.systemBackground).opacity(Constants.backgroundOpacity))
             }
         }
     }
     
     // MARK: - Scoreboard
     
+    /// Displays the coint for the number of coins you have thrown, how many hit La Rana, and how many hit the target (its mouth)
     private var scoreBoard: some View {
         VStack {
             Text("Score")
                 .font(.callout)
             HStack {
-                Text("Throws: \(nThrows)")
-                Text("Hit La Rana: \(nHitsLaRana)")
-                Text("In the Hole: \(nHitsTarget)")
+                scoreContent(text: "Throws", score: nThrows)
+                scoreContent(text: "Hits", score: nHitsLaRana)
+                scoreContent(text: "Targets", score: nHitsTarget)
             }
             .font(.caption)
         }
-        .padding(8)
-        .background {
-            RoundedRectangle(cornerRadius: 12)
-                .foregroundColor(Color(UIColor.systemBackground))
+    }
+    
+    /// A stack of the scoring category and its integer value
+    private func scoreContent(text: String, score: Int) -> some View {
+        VStack {
+            Text(text)
+                .font(.caption2)
+            Text(score.description)
+                .font(.subheadline)
         }
+        .frame(width: Constants.textContentWidth)
     }
     
     // MARK: - Buttons
      
+    /// A button that responds to itself being active by becoming prominent, defaults to bordered
     @ViewBuilder
-    private func OverlayButton(text: String, activeForState: GameState, action: @escaping () -> Void) -> some View {
+    private func OverlayButton(
+        text: String,
+        systemName: String? = nil,
+        activeForState: GameState,
+        action: @escaping () -> Void
+    ) -> some View {
         if state == activeForState {
             Button(action: { action() }) {
-                Text(text)
+                OverlayButtonContent(text: text, systemName: systemName)
             }
-            .frame(width: Constants.buttonWidth)
             .buttonStyle(.borderedProminent)
         } else {
             Button(action: { action() }) {
-                Text(text)
+                OverlayButtonContent(text: text, systemName: systemName)
             }
-            .frame(width: Constants.buttonWidth)
             .buttonStyle(.bordered)
         }
-        
     }
     
+    /// Content of the button that can be either an icon or a string.
+    /// If its an icon, the string displays in its context menu.
+    @ViewBuilder
+    private func OverlayButtonContent(text: String, systemName: String?) -> some View {
+        if let systemName {
+            Image(systemName: systemName)
+                .contextMenu {
+                    Text("Tap to \(text)")
+                }
+        } else {
+            Text(text)
+                .frame(width: Constants.textContentWidth)
+        }
+    }
+    
+    /// The button that will make the table reset to the nearest/best anchor position
     private var resetAnchorButton: some View {
-        OverlayButton(text: "Reset\nAnchor", activeForState: .resetting, action: { onTapReset() })
+        OverlayButton(text: "Reset Anchor", systemName: "arrow.counterclockwise", activeForState: .resetting, action: { onTapReset() })
     }
     
+    /// The button that will allow the user to drag the table to a new position
     private var moveTableButton: some View {
-        OverlayButton(text: "Move\nTable", activeForState: .move, action: { onTapMove() })
+        OverlayButton(text: "Move Table", systemName: "arrow.up.and.down.and.arrow.left.and.right", activeForState: .move, action: { onTapMove() })
     }
     
     // MARK: - Constants
     
     private struct Constants {
-        static let buttonSpacing: CGFloat = 0
-        static let buttonWidth: CGFloat = 96
+        static let buttonSpacing: CGFloat = 12
+        static let textContentWidth: CGFloat = 64
+        static let backgroundPadding: CGFloat = 8
+        static let backgroundOpacity = 0.5
+        static let backgroundRadius: CGFloat = 16
     }
 }
 
